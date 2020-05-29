119d83e7dc8f42b10c0c4b9efb8825a639e9f805
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-05-28 21:43:43 -0500

    feat: filter works

---
+++ SG.js
@@ -0,0 +1,66 @@
+
+"use strict";
+
+$(document).ready(function () {
+    var url = "https://ghibliapi.herokuapp.com/films/";
+    //sending request to server
+    $.ajax(url, {
+        type: "GET"
+    }).done(function (data, index) {
+
+        renderMovies();
+
+        // render html//
+        function renderMovies() {
+            for (let i = 0; i < data.length; i++) {
+                let movieTitleHTML = `
+                                  <div class="col-sm-4">
+                                      <div class="card mt-3">
+                                           <div class="card-body pt-2">
+                                              <h5 class="card-title">${data[i].title}</h5>
+                                               <p class="card-text">${data[i].description}</p>
+                                               <p class="card-text">Date released: ${data[i].release_date}</p>
+                                               <p class="card-text">Director: ${data[i].director}</p>
+                                               <p class="card-text">Rotten Tomatoes Score: ${data[i].rt_score}</p>
+                                            </div>
+                                       </div>
+                                  </div>`;
+                $('.row').append(movieTitleHTML);
+            }
+        }
+
+
+        function filterMovies() {
+            let searchTerm = $('#search-term').val();
+            console.log(searchTerm);
+            for (let i = 0; i < data.length; i++){
+                if (searchTerm === data[i].title){
+                    $('.row').hide();
+                    let filterMovieTitleHTML =
+                        `<div class="col-sm-1">
+                                      <div class="card mt-3">
+                                           <div class="card-body pt-2">
+                                              <h5 class="card-title">${data[i].title}</h5>
+                                               <p class="card-text">${data[i].description}</p>
+                                               <p class="card-text">Date released: ${data[i].release_date}</p>
+                                               <p class="card-text">Director: ${data[i].director}</p>
+                                               <p class="card-text">Rotten Tomatoes Score: ${data[i].rt_score}</p>
+                                            </div>
+                                       </div>
+                                  </div>`;
+                    $('.row1').append(filterMovieTitleHTML);
+                }
+            }
+        }
+        $('#do-search').click(function(e){
+            e.preventDefault();
+            filterMovies();
+        });
+    }).fail(function (error) {
+        console.error(error);
+    }).always(function () {
+        console.log('you always run once you get a response')
+    });
+});
+
+
